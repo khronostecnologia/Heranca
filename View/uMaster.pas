48be25ab5555cd4f9a6695b6f85716e1e0755419
Ruan Gabriel <36700730+rruandavid@users.noreply.github.com>
2024-06-29 17:53:06 -0300

    Implementações
    - Implementado nova rotina para controlar componentes de MasterDetalhes;

--- View/uMaster.pas
+++ View/uMaster.pas
@@ -20,6 +20,7 @@ type
   protected
     { Protected declarations }
     procedure ControlaEditsMaster(AEnabled: Boolean);
+    procedure ControlaEditsMasterDetalhes(const pTagValue : Integer; Const AEnabled : Boolean);
   private
     { Private declarations }
   public
@@ -107,4 +108,39 @@ begin
   end;
 end;
 
+procedure TFrmMaster.ControlaEditsMasterDetalhes(const pTagValue : Integer; Const AEnabled : Boolean);
+var
+  i : Integer;
+  J : Integer;
+begin
+  {Cada detalhe precisa ter uma tag diferente para somente os objetos dessa tag sejam manipulados}
+  for I := 0 to Pred(Self.ComponentCount) do
+  begin
+    if not Self.Components[I].Tag = pTagValue  then
+      Continue;
+
+    if Self.Components[I] is TDBMemo then
+        TDBMemo(Self.Components[I]).Enabled         := AEnabled;
+    if Self.Components[I] is TDBEdit then
+      TDBEdit(Self.Components[I]).Enabled           := AEnabled;
+    if Self.Components[I] is TDBComboBox then
+      TDBComboBox(Self.Components[I]).Enabled       := AEnabled;
+    if Self.Components[I] is TJvDBDateEdit then
+      TJvDBDateEdit(Self.Components[I]).Enabled     := AEnabled;
+    if Self.Components[I] is TJvDBCheckBox then
+      TJvDBCheckBox(Self.Components[I]).Enabled     := AEnabled;
+    if Self.Components[I] is TJvDBMaskEdit then
+      TJvDBMaskEdit(Self.Components[I]).Enabled     := AEnabled;
+    if Self.Components[I] is TJvDBCalcEdit then
+      TJvDBCalcEdit(Self.Components[I]).Enabled     := AEnabled;
+    if Self.Components[I] is TDBLookupComboBox then
+      TDBLookupComboBox(Self.Components[I]).Enabled := AEnabled;
+    if Self.Components[I] is TRxDBCalcEdit then
+      TRxDBCalcEdit(Self.Components[I]).Enabled     := AEnabled;
+    if Self.Components[I] is TDBRadioGroup then
+      TDBRadioGroup(Self.Components[I]).Enabled     := AEnabled;
+    if Self.Components[I] is TDBImage then
+      TDBImage(Self.Components[I]).Enabled          := AEnabled;
+  end;
+end;
 end.
